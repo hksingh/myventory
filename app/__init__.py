502b98c865324de733c79b9500ed0e3fc1404686
hemant singh <hsingh@impact-guard.com>
2023-11-26 22:29:02 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+# Imports from Flask
+from flask import Flask, render_template
+# Other imports
+import os
+import config
+import re
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app_env = os.environ.get("FLASK_ENV")
+
+if app_env is None:
+    app_env = "Development"
+
+def create_app(config_env=app_env):
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return render_template('home.html')
+
+    @app.route("/test")
+    def test():
+        return render_template('test.html')
+    return app
